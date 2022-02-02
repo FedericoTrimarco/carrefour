5d73b7e694bcb7a174cb2b7f13fe3e848caeef62
stefanofilippini <filippini.stefano96@gmail.com>
2022-02-02 17:49:43 +0100

    added update products

--- resources/views/admin/input.blade.php
+++ resources/views/admin/input.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <h2>
-        New Product
+        Update Product
     </h2>
 
     @if ($errors->any())
@@ -22,21 +22,22 @@
         @csrf
 
         <label for="brand">Marca:</label>
-        <input type="text" name="brand" placeholder="insert brand"><br>
+        <input type="text" name="brand" placeholder="insert brand" value="{{ $product -> brand }}"><br>
         <label for="name_product">nome del prodotto:</label>
-        <input type="text" name="name_product" placeholder="insert name_product"><br>
+        <input type="text" name="name_product" placeholder="insert name_product" value="{{ $product -> name_product }}"><br>
         <label for="price">prezzo:</label>
-        <input type="number" name="price" step="0.01" min="0"><br>
+        <input type="number" name="price" step="0.01" min="0" value="{{ $product -> price }}"><br>
         <label for="price_detail">prezzo al dettaglio:</label>
-        <input type="text" name="price_detail" placeholder="insert price_detail"><br>
+        <input type="text" name="price_detail" placeholder="insert price_detail" value="{{ $product -> price_detail }}"><br>
         <label for="description">breve descrizione:</label>
-        <input type="text" name="description" placeholder="insert description"><br>
+        <input type="text" name="description" placeholder="insert description" value="{{ $product -> description }}"><br>
         <label for="thumb">link all'immagine:</label>
-        <input type="text" name="thumb" placeholder="insert thumb"><br>
+        <input type="text" name="thumb" placeholder="insert thumb" value="{{ $product -> thumb }}"><br>
         <label for="is_new">nuovo prodotto:</label>
-        <input type="checkbox" name="is_new"><br>
+        <input type="checkbox" name="is_new" value="{{ $product -> is_new }}"><br>
         <input type="submit">
     </form>
+
 
     {{-- nomi colonne database
     brand / string
