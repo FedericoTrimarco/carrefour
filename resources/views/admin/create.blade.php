7f06f6c2f753e429256e00b563254ff572276039
stefanofilippini <filippini.stefano96@gmail.com>
2022-02-02 18:06:07 +0100

    added bootstrap class to create new product

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -21,21 +21,21 @@
         @method('POST')
         @csrf
 
-        <label for="brand">Marca:</label>
-        <input type="text" name="brand" placeholder="insert brand"><br>
-        <label for="name_product">nome del prodotto:</label>
-        <input type="text" name="name_product" placeholder="insert name_product"><br>
-        <label for="price">prezzo:</label>
-        <input type="number" name="price" step="0.01" min="0"><br>
-        <label for="price_detail">prezzo al dettaglio:</label>
-        <input type="text" name="price_detail" placeholder="insert price_detail"><br>
-        <label for="description">breve descrizione:</label>
-        <input type="text" name="description" placeholder="insert description"><br>
-        <label for="thumb">link all'immagine:</label>
-        <input type="text" name="thumb" placeholder="insert thumb"><br>
-        <label for="is_new">nuovo prodotto:</label>
-        <input type="checkbox" name="is_new"><br>
-        <input type="submit">
+        <label for="brand" class="form-label">Marca:</label>
+        <input type="text" name="brand" placeholder="insert brand" class="form-control"><br>
+        <label for="name_product" class="form-label">nome del prodotto:</label>
+        <input type="text" name="name_product" placeholder="insert name_product" class="form-control"><br>
+        <label for="price" class="form-label">prezzo:</label>
+        <input type="number" name="price" step="0.01" min="0" class="form-control"><br>
+        <label for="price_detail" class="form-label">prezzo al dettaglio:</label>
+        <input type="text" name="price_detail" placeholder="insert price_detail" class="form-control"><br>
+        <label for="description" class="form-label">breve descrizione:</label>
+        <input type="text" name="description" placeholder="insert description" class="form-control"><br>
+        <label for="thumb" class="form-label">link all'immagine:</label>
+        <input type="text" name="thumb" placeholder="insert thumb" class="form-control"><br>
+        <label for="is_new" class="form-check-label">nuovo prodotto:</label>
+        <input type="checkbox" name="is_new" class="form-check-input"><br>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
     {{-- nomi colonne database
