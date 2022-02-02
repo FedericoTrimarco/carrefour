edbf3fae7dfdf3e8d96cf90bf03c90bb8b25a1b2
GvMazzon25 <gvmazzon25@gmail.com>
2022-02-02 17:57:15 +0100

    show-card

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container d-flex justify-content-center my-5">
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
@@ -9,9 +9,13 @@
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <h4 class="card-title">{{ Auth::product()->brand }}</h4>
+              <h5 class="card-title">{{ Auth::product()->name_product }}</h5>
+              <p class="card-text">{{ Auth::product()->description }}</p>
+              <div>
+                <p class="card-text mb-0"><small class="text-muted">{{ Auth::product()->price_detail }}</small></p>
+                <p class="card-text">€ {{ Auth::product()->price }} </p>
+              </div>
             </div>
           </div>
         </div>
